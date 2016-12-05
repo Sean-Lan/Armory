b3b2452315be97365425c0ea312132e65b7850ea
Sean-Lan <sean.lan.thu@gmail.com>
2016-12-05 11:21:26 +0800

    Comment: add problem link of evaluate division.

--- UnionFind/evaluateDivision.cpp
+++ UnionFind/evaluateDivision.cpp
@@ -3,6 +3,8 @@
  * Sean
  * 2016-11-16
  *
+ * https://leetcode.com/problems/evaluate-division/
+ *
  * Equations are given in the format A / B = k, where A and B are variables represented as strings, and k is a real number (floating point number). Given some queries, return the answers. If the answer does not exist, return -1.0.
  *
  * Example:
